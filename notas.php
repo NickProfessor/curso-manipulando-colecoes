e6f8af665b0c02771d84a2eed71f80ba5ba3831c
Nickollas <nickollassilva90@gmail.com>
2025-01-30 22:32:49 -0300

    aula 3

--- notas.php
+++ notas.php
@@ -3,7 +3,7 @@ $notas = [
     "Ana" => 6,
     "Bruno" => 8,
     "Maria" => 10,
-    "Daniel" => 8
+    "Daniel" => 7
 ];
 
 asort($notas);
@@ -12,3 +12,17 @@ asort($notas);
 // ksort e krsort ordenam de forma crescente e descrescente levando em conta as CHAVES
 ksort($notas);
 var_dump($notas);
+
+// gettype() checa o tipo do dado
+
+//is_array verifica se é um array
+//array_is_list verifica se é um array cm chaves númerico sem pular nenhum número
+
+var_dump(array_key_exists('Maria', $notas)); // Verifica se essa chave existe no array
+
+var_dump(isset($notas['Daniel'])); //Verifica se o valor da chave está definido e não é nulo
+
+var_dump(in_array(0, $notas)); // pode ser adicionado um terceiro parâmetro para definir se o número deve ser strict
+//in_array verifica se o valor existe no array
+
+echo array_search(10, $notas); // tambem pode definir se a busca deve ser strict ou não
